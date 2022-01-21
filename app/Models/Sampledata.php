bf2d974448002c89979a98f0e6454c30495c9e6b
rajeshofficial1432 <rajesh.webilok@gmail.com>
2022-01-21 15:49:08 +0530

    First Livewire

---
+++ app/Models/Sampledata.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Model;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Sampledata extends Model
+{
+    protected $fillable = ['first_name', 'last_name', 'gender'];
+}
+
+?>
